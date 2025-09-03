ee9d874d00cda3c099f8c47ded595a811ae83cdb
Onkar Gulhane <omkar288113@gmail.com>
2025-09-03 19:56:40 +0530

    Add files via upload

---
+++ calculation.java
@@ -0,0 +1,14 @@
+class Calculation{
+
+    public static void main(String args[]){
+        Utility ob = new Utility();
+        int ans;
+        ans = ob.factorial(5);
+        System.out.println("Factorial of 5:"+ans);
+
+        ans = ob.power(3,4);
+        System.out.println("Power of 3 rais to 4 is:"+ans);
+    }
+}
+    
+
